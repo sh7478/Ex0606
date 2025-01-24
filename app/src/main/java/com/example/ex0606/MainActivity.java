f251b5936b4ee115780c707e7556eb62a5c21f48
sh7478 <sh7478@bs.amalnet.k12.il>
2025-01-24 20:37:54 +0200

    added function for onClick and fixed button text

--- app/src/main/java/com/example/ex0606/MainActivity.java
+++ app/src/main/java/com/example/ex0606/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex0606;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    ImageView imageV;
+    int randomNum;
+    Random rnd = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        imageV = findViewById(R.id.imageV);
+    }
+
+    public void clicked(View view)
+    {
+        randomNum = rnd.nextInt((3-1+1)+1);
+        btn.setText("The number that was rolled is-> " + randomNum);
+
     }
 }
